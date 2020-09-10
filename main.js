09c67035e3c16d19ece939dfe0bd29f8e41a0a6f
teekaytech <taofeek.olalere@feghas.com>
2020-09-10 15:07:30 +0100

    Creat a class for all pages to extend

--- main.js
+++ main.js
@@ -94,7 +94,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_pageHeader__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/pageHeader */ \"./src/modules/pageHeader.js\");\n/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/tabs */ \"./src/modules/tabs.js\");\n\n\n\nconst pageTabs = Array.from(_modules_tabs__WEBPACK_IMPORTED_MODULE_1__[\"default\"].tabs);\n\nconst render = () => {\n  const parentContainer = document.getElementById('content');\n\n  parentContainer.appendChild(_modules_pageHeader__WEBPACK_IMPORTED_MODULE_0__[\"default\"].headerContainer);\n};\n\nrender();\n\npageTabs.forEach(tab => {\n  tab.addEventListener('click', () => {\n    alert(tab.innerText);\n  });\n});\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_pageHeader__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/pageHeader */ \"./src/modules/pageHeader.js\");\n/* harmony import */ var _modules_tabs__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/tabs */ \"./src/modules/tabs.js\");\n/* harmony import */ var _modules_homePage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/homePage */ \"./src/modules/homePage.js\");\n\n\n\n\nconst pageTabs = Array.from(_modules_tabs__WEBPACK_IMPORTED_MODULE_1__[\"default\"].tabs);\n\nconst render = () => {\n  const parentContainer = document.getElementById('content');\n\n  parentContainer.appendChild(_modules_pageHeader__WEBPACK_IMPORTED_MODULE_0__[\"default\"].headerContainer);\n};\n\nrender();\n\npageTabs.forEach(tab => {\n  tab.addEventListener('click', () => {\n    alert(tab.innerText);\n  });\n});\n\nconst home = new _modules_homePage__WEBPACK_IMPORTED_MODULE_2__[\"default\"]('home-page');\nconsole.log(home);\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -106,6 +106,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
 /***/ (function(module, exports) {
 
 eval("const CreateElement = (name, text, id = '', classname = '') => {\n  const element = document.createElement(name);\n\n  element.innerText = text;\n  element.className += classname;\n  element.setAttribute('id', id);\n\n  return element;\n};\n\nmodule.exports = CreateElement;\n\n//# sourceURL=webpack:///./src/modules/domElements.js?");
+
+/***/ }),
+
+/***/ "./src/modules/homePage.js":
+/*!*********************************!*\
+  !*** ./src/modules/homePage.js ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _pagesTemplate__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./pagesTemplate */ \"./src/modules/pagesTemplate.js\");\n// import CreateElement from './domElements';\n\n\nclass HomePage extends _pagesTemplate__WEBPACK_IMPORTED_MODULE_0__[\"default\"] {\n  // createContainer() {\n  //   const container = CreateElement('div', '', this.name, this.name);\n  //   return container;\n  // }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (HomePage);\n\n\n//# sourceURL=webpack:///./src/modules/homePage.js?");
 
 /***/ }),
 
@@ -121,6 +133,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom
 
 /***/ }),
 
+/***/ "./src/modules/pagesTemplate.js":
+/*!**************************************!*\
+  !*** ./src/modules/pagesTemplate.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./domElements */ \"./src/modules/domElements.js\");\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_domElements__WEBPACK_IMPORTED_MODULE_0__);\n\n\nclass Page {\n  constructor(name) {\n    this.name = name;\n  }\n\n  createContainer() {\n    const container = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('div', '', this.name, this.name);\n    return container;\n  }\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Page);\n\n\n//# sourceURL=webpack:///./src/modules/pagesTemplate.js?");
+
+/***/ }),
+
 /***/ "./src/modules/tabs.js":
 /*!*****************************!*\
   !*** ./src/modules/tabs.js ***!
@@ -129,7 +153,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dom
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./domElements */ \"./src/modules/domElements.js\");\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_domElements__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst createTabs = (() => {\n  const nav = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('nav', '', 'navbar', 'navbar');\n  const tabList = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('ul', '', 'tabs');\n  const navItems = ['Home', 'Our Menu', 'Get in touch'];\n\n  navItems.forEach((tab) => {\n    const list = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('li', tab, 'list', 'list');\n    tabList.appendChild(list);\n  });\n\n\n  nav.appendChild(tabList);\n  const tabs = tabList.children;\n\n  return { nav, tabs };\n})();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (createTabs);\n\n\n//# sourceURL=webpack:///./src/modules/tabs.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./domElements */ \"./src/modules/domElements.js\");\n/* harmony import */ var _domElements__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_domElements__WEBPACK_IMPORTED_MODULE_0__);\n\n\nconst createTabs = (() => {\n  const nav = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('nav', '', 'navbar', 'navbar');\n  const tabList = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('ul', '', 'tabs');\n  const navItems = ['Home', 'Our Menu', 'Get in touch'];\n\n  const doTabs = (items, list) => {\n    for (let i = 0; i < items.length; i += 1) {\n      const li = _domElements__WEBPACK_IMPORTED_MODULE_0___default()('li', items[i], 'list', `list${i + 1}`);\n      li.setAttribute('data-val', i);\n      list.appendChild(li);\n    }\n    return list;\n  };\n\n\n  nav.appendChild(doTabs(navItems, tabList));\n  const tabs = tabList.children;\n\n  return { nav, tabs };\n})();\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (createTabs);\n\n\n//# sourceURL=webpack:///./src/modules/tabs.js?");
 
 /***/ })
 
